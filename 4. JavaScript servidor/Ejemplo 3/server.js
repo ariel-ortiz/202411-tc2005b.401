b6b6ac303a1418b574145eccc13b031bb145fd0a
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-03-16 00:52:03 +0000

    Update server code to respond a full HTML document

--- 4. JavaScript servidor/Ejemplo 3/server.js
+++ 4. JavaScript servidor/Ejemplo 3/server.js
@@ -4,9 +4,22 @@ const port = 8080;
 const ipAddr = '52.20.170.244';
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 418;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('I am Groot\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>I am Groot</title>
+      </head>
+      <body>
+        <h1>Hello Groot!</h1>
+        <p>
+          This is a test.
+        </p>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(port, () => {
